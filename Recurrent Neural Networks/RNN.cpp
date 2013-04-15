446ff28d503431346096a3fe2fdfbbad637986a4
RichyChen <cmm620@gmail.com>
2013-04-15 18:54:43 +0800

    second commit

---
+++ Recurrent Neural Networks/RNN.cpp
@@ -0,0 +1,16 @@
+/*
+ * RNN.cpp
+ *
+ *  Created on: 2013-4-15
+ *      Author: chenmm
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	cout << "This is a start!" << endl;
+	return 0;
+}
+
---
+++ Recurrent Neural Networks/RNN.h
@@ -0,0 +1,14 @@
+/*
+ * RNN.h
+ *
+ *  Created on: 2013-4-15
+ *      Author: chenmm
+ */
+
+#ifndef RNN_H_
+#define RNN_H_
+
+
+
+
+#endif /* RNN_H_ */
